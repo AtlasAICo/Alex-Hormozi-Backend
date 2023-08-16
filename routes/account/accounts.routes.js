e65d4c791213db747c9db07ba16944c4d7e546b9
cocomatias <matiascoco99@gmail.com>
2023-08-16 15:25:20 -0300

    feat: add delete user route

--- routes/account/accounts.routes.js
+++ routes/account/accounts.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { Data } = require("./../../model/Data");
+const User = require("../../model/User");
 
 const router = express.Router();
 
@@ -43,6 +44,36 @@ router.get("/", async (req, res) => {
     res.json(data);
   } catch (error) {
     res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+// Only works if the token is present and matches the secret key
+router.delete("/:email", async (req, res) => {
+  // Extract the token from the Authorization header
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1]; // Split the header by space and get the second part (the token)
+
+  // Check if the token is present and matches the secret key
+  if (!token || token !== process.env.secret) {
+    return res.status(401).json({ message: "Unauthorized access" });
+  }
+
+  const email = req.params.email;
+  if (!email) {
+    return res.status(400).json({ message: "Email required" });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    await User.deleteOne({ email });
+    return res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error(error); // Log the error for debugging
+    return res
+      .status(500)
+      .json({ message: "An error occurred while deleting the user" });
   }
 });
 
